ccb1a0f0b6e779ecf3c977e6b5cb0be81866ef0d
Enrique Ramirez <dev@indiecodelabs.com>
2008-11-10 16:14:36 -0400

    Added icons and images for branding.

---
+++ res/resource.h
@@ -0,0 +1,50 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by screendump.rc
+//
+#define IDD_screendump_DIALOG           102
+#define IDD_ABOUTDIALOG                 103
+#define IDD_REGIONSELECT                104
+#define IDD_REGIONDIALOG                105
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDD_SETTINGSDLG                 130
+#define IDD_AUTOTIMER                   132
+#define IDI_ICON1                       133
+#define IDI_ICONTRAY                    133
+#define IDC_RADIOBMP                    1000
+#define IDC_RADIOTIFF                   1001
+#define IDC_RADIOPNG                    1002
+#define IDC_RADIOJPEG                   1003
+#define IDC_TXTQUALITY                  1004
+#define IDC_EDIT2                       1005
+#define IDC_TXTDESTINATION              1005
+#define ID_TXTACDELAY                   1005
+#define IDC_BTNBROWSE                   1006
+#define IDC_CHKAUTONAME                 1007
+#define IDC_CHKHOG                      1010
+#define ID_OPTSECONDS                   1013
+#define ID_OPTMILLISECONDS              1014
+#define ID_OPTFULLSCREEN                1015
+#define ID_OPTFOREGROUND                1016
+#define ID_BTNSTART                     1017
+#define IDC_IMGBACKGROUND               1018
+#define IDC_ERRORMSG                    1020
+#define ID_TRAY_ABOUT                   32773
+#define ID_TRAY_OPTIONS                 32774
+#define ID_TRAY_EXIT                    32775
+#define ID_Menu                         32776
+#define ID_TRAY_OPENDEST                32777
+#define ID_TRAY_AUTOCAPTURE             32778
+#define ID_TRAY_REGION                  32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           106
+#endif
+#endif
